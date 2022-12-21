a5c83eb25f5170e2e147c020ef4a413f874f7580
kizey <ikizey@icloud.com>
2022-12-22 00:26:08 +0400

    add BookList component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
-function App() {
-  return <div>App</div>;
-}
+import BookList from './components/BookList';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Books:</h1>
+      <BookList />
+    </div>
+  );
+};
 
 export default App;
