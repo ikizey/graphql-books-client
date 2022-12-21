29864670413a8f6ce474219d5c93d880dcc60251
kizey <ikizey@icloud.com>
2022-12-22 00:56:22 +0400

    setup apollo-client

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,10 +1,30 @@
 import React from 'react';
+import { useQuery, gql } from '@apollo/client';
+
+const GET_BOOKS = gql`
+  query getBooks {
+    books {
+      id
+      name
+      authors {
+        name
+      }
+    }
+  }
+`;
 
 const BookList = () => {
+  const { loading, error, data } = useQuery(GET_BOOKS);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error : {error.message}</p>;
+
   return (
     <div>
       <ul>
-        <li>Book</li>
+        {data.books.map((book) => (
+          <li key={book.id}>{book.name}</li>
+        ))}
       </ul>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import './index.css';
 import App from './App';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4321',
+
+  cache: new InMemoryCache(),
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </StrictMode>
 );
